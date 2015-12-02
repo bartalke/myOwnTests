6ea3f4e012fd22bb0e2eca3f8ed976a9eb1e0a9a
Bartlomiej Blachut <bartlomiej.blachut@nokia.com>
2015-12-02 17:13:43 +0100

    add printing in test fun

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -3,12 +3,18 @@
 
 void test()
 {
+    std::vector<int> v;
 
+    for (int i = 0; i <= v.size(); ++i)
+    {
+        std::cout << v[i];
+    }
 }
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+
     test();
     return 0;
 }
